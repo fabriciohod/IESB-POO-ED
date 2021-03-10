17c657284127a9b9d57d6f1eb4e1ba70fb5f1f59
Fabricio Duarte <fabriciohod@hotmail.com>
2021-03-10 16:43:26 -0300

    Finalizando Lab1

---
+++ Lab1/Person.cs
@@ -0,0 +1,41 @@
+using System;
+
+namespace Lab1
+{
+  public class Person
+  {
+    public string Nome { get; private set; }
+    public int Idade { get; private set; }
+    public float Altura { get; private set; }
+    public float Peso { get; private set; }
+    private string situacao_IMC = null;
+    public string Situacao_IMC
+    {
+      get => situacao_IMC is null ? GetPersonIMC() : situacao_IMC; 
+      private set => situacao_IMC = value;
+    }
+
+    public Person(string nome, int idade, float peso, float altura) =>
+        (Nome, Idade, Peso, Altura) = (nome, idade, peso, altura);
+
+    private string GetPersonIMC()
+    {
+      float IMC = (Peso / (Altura * Altura));
+
+      if (IMC > 18.5f && IMC < 24.9f)
+        return Situacao_IMC = "Parabéns — você está em seu peso normal!";
+
+      if (IMC > 25.0f && IMC < 29.9f)
+        return Situacao_IMC = "Você está acima de seu peso (sobrepeso).";
+
+      if (IMC > 30.0f && IMC < 34.9f)
+        return Situacao_IMC = "Obesidade grau I.";
+
+      if (IMC > 35.0f)
+        return Situacao_IMC = "Obesidade grau II.";
+
+      else
+        return Situacao_IMC = "Obesidade graus III e IV.";
+    }
+  }
+}
---
+++ Lab1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person( "Bob Nelson", 37, 78, 1.70f );
+            Person person2 = new Person( "Testolfo Rocha", 47, 60, 1.65f );
+            Person person3 = new Person( "Lisa Leite", 47, 92, 1.72f );
+
+            Console.WriteLine($"{person1.Nome} | {person1.Situacao_IMC}");
+            Console.WriteLine($"{person2.Nome} | {person2.Situacao_IMC}");
+            Console.WriteLine($"{person3.Nome} | {person3.Situacao_IMC}");
+        }
+    }
+}
